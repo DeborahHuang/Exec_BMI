e3d4478cfc79e07c720f6cd715e2e274226f11ad
88693 <88693@LAPTOP-FS9ST1LD>
2022-10-24 23:15:28 +0800

    加入專案檔案。

---
+++ Exec_BMI/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exec_BMI
+{//計算BMI
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			double height = 160;
+			double weight = 52;
+			double BMI = weight / Math.Pow((height / 100),2);
+			Console.WriteLine(BMI);
+		}
+	}
+}
